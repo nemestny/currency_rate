a76c35bb3670402de27cdd315fab34e9ed3a9f43
Aleksandr Noskov <nemestny@politeh.ru>
2019-03-13 13:50:08 +0500

    Generate model for Rates

---
+++ db/migrate/20190313084331_create_rates.rb
@@ -0,0 +1,11 @@
+class CreateRates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rates do |t|
+      t.decimal :value, precision: 8, scale: 4, null: false
+      t.string :type
+      t.timestamp :expires_at
+
+      t.timestamps
+    end
+  end
+end
