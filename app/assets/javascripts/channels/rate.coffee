1f7378d782eef32330ab356a630fff72efab271f
Aleksandr Noskov <nemestny@politeh.ru>
2019-03-14 21:22:50 +0500

    Add create Forced_rate

--- app/assets/javascripts/channels/rate.coffee
+++ app/assets/javascripts/channels/rate.coffee
@@ -8,5 +8,5 @@ App.rate = App.cable.subscriptions.create "RateChannel",
     # Called when there's incoming data on the websocket for this channel
 
   send_rate: ->
-    # Вызывает `AppearanceChannel#appear(data)` на сервере.
-    @perform("send_rate", sending_rate: $("main").data("appearing-on"))
+    # Вызывает `RateChannel#send_rate(data)` на сервере.
+    @perform("send_rate", sending_rate: $("main").data())
--- app/controllers/forced_rates_controller.rb
+++ app/controllers/forced_rates_controller.rb
@@ -1,5 +1,17 @@
 class ForcedRatesController < ApplicationController
   def new
-    FetchRateJob.perform_later
+    @forced_rate = ForcedRate.last || ForcedRate.new
+    # FetchRateJob.perform_later
+  end
+
+  def create
+    ForcedRate.create(value: permit_params[:value], expires_at: permit_params[:expires_at])
+    render :new
+  end
+
+  private
+
+  def permit_params
+    params.require(:forced_rate).permit(:value, :expires_at)
   end
 end
