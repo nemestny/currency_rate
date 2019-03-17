f2e3987e464f8bf323b7673aaf22ccc37ab76369
Aleksandr Noskov <nemestny@politeh.ru>
2019-03-17 17:00:20 +0500

    Add current method to Rate model

--- app/channels/rate_channel.rb
+++ app/channels/rate_channel.rb
@@ -1,21 +1,9 @@
 class RateChannel < ApplicationCable::Channel
   def subscribed
-    # forced = ForcedRate.last
-    # if forced && forced.expires_at > Time.now
-    #   rate = forced
-    # else
-    #   rate = FetchedRate.last
-    # end
-    # test = {value: 'test'}
-    # stream_from test
-    # rate = {value: 'deddd'}
-    # RateChannel.broadcast_to 'rate', Rate.last
     stream_from 'rate'
   end
 
   def send_rate(*args)
-    puts '+'*10
-    ActionCable.server.broadcast('rate',Rate.last)
-    puts '-'*10
+    ActionCable.server.broadcast('rate',Rate.new.current)
   end
 end
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,22 @@
 class Rate < ApplicationRecord
 
-  after_create_commit {ActionCable.server.broadcast('rate',self)}
+  after_create_commit {ActionCable.server.broadcast('rate', self.current)}
+
+  def current
+    if forced && (forced.expires_at.utc > Time.now.utc)
+      forced
+    else
+      fetched
+    end
+  end
+
+  private
+
+  def forced
+    ForcedRate.last
+  end
+
+  def fetched
+    FetchedRate.last
+  end
 end
